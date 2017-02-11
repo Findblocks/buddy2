17755b06eb5801675608b76dad983c4d89741ea0
Findblocks <findblocks@gmail.com>
2017-02-11 22:27:52 +0000

    .up

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Buddy2-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "Buddy2URI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
